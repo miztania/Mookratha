af10ff380e653dca50b140a435ebbb2a1b5c087b
Rain Maker <108649582+KapiGameMaker@users.noreply.github.com>
2023-05-29 21:31:19 +0700

    timer

---
+++ Mookratha/Assets/Scripts/Manager/Timer.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [Header("Component")]
+    public TextMeshProUGUI minuteText;
+    public TextMeshProUGUI secondText;
+
+    [Header("Timer Setting")]
+    public float currentTime;
+    public bool countDown;
+
+    [Header("Timer Setting")]
+    public float timerLimit;
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        if (countDown && currentTime > 0)
+        {
+            currentTime -= Time.deltaTime;
+            minuteText.text = ((int)currentTime / 60).ToString() ;
+            secondText.text = returnSecond((int)currentTime) ;
+        }    
+    }
+
+    private string returnSecond(int currentTime)
+    {
+        string returnString;
+        returnString = (currentTime % 60).ToString();
+        if (currentTime % 60 < 10) returnString = "0" + returnString;
+        return returnString;
+    }
+}
